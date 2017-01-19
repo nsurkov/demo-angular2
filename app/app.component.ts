94a81103ae8b51f3f2c929f97c128bffd668ff4e
nikolay.surkov <nikolay.surkov@exactprosystems.com>
2017-01-19 15:24:51 +0300

    added counter demo

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,65 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `
+
+  <h2>Parameters</h2>
+  <div>
+  <div>
+    <label>Min value:</label>
+    <input [(ngModel)]="minValue">
+  </div>
+  <div>
+    <label>Max value:</label>
+    <input [(ngModel)]="maxValue">
+  </div>
+  </div>
+
+  <h2>Inside Form</h2>
+  <form [formGroup]="form">
+    <div>
+      <label>Tune value:</label>
+      <counter
+        formControlName="counter"
+        [max]="maxValue"
+        [min]="minValue"
+      ></counter>
+    </div>
+    <div>
+      <p *ngIf="!form.valid">Form invalid value</p>
+      <p *ngIf="form.valid">Form valid value</p>
+      <pre>{{ form.value | json }}</pre>
+    </div>
+  </form>
+
+  <h2>Standalone</h2>
+  <div>
+    <label>Tune value:</label>
+    <counter
+      [max]="maxValue"
+      [min]="minValue"
+      [(value)]="counterValue"
+    ></counter>
+  </div>
+  <div>
+    <p>{{ counterValue }}</p>
+  </div>
+  `
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  implements OnInit {
+  form:FormGroup;
+  counterValue: number = 10;
+  minValue: number = 0;
+  maxValue:number = 12;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit() {
+    this.form = this.fb.group({
+      counter: this.counterValue
+    });
+  }
+
+}
--- app/app.module.ts
+++ app/app.module.ts
@@ -1,11 +1,15 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { CommonModule }       from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppComponent }  from './app.component';
+//import { SharedModule }  from './shared/shared.module';
 
+import { CounterComponent }  from './shared/counter.component';
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
+  imports:      [ BrowserModule,  CommonModule, FormsModule, ReactiveFormsModule /*, SharedModule*/ ],
+  declarations: [ AppComponent, CounterComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
